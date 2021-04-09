62dab53865165856e386eff0cb6eac47e611de5e
Brian <blpmurphy@gmail.com>
2021-04-09 19:34:51 +0100

    Function declarations vs expressions lesson and assignment

--- assignments.js
+++ assignments.js
@@ -1,3 +1,4 @@
+// 1: Functions
 function describeCountry(country, population, capitalCity) {
     return (`${country} has ${population} million people and its capital city is ${capitalCity}.`);
 }
@@ -5,6 +6,29 @@ function describeCountry(country, population, capitalCity) {
 const countryIreland = describeCountry("Ireland", 5, "Dublin");
 const countryFinland = describeCountry("Finland", 6, "Helsinki");
 const countryPortugal = describeCountry("Portugal", 12, "Lisbon");
-console.log(countryIreland);
-console.log(countryFinland);
-console.log(countryPortugal);
+// console.log(countryIreland);
+// console.log(countryFinland);
+// console.log(countryPortugal);
+
+// 2: Function Declarations vs. Expressions
+
+const worldPopulation = 7900;
+
+// Function Declarations
+function percentageOfWorld1(population) {
+    return population / worldPopulation * 100;
+}
+// Function Expression
+const percentageOfWorld2 = function (population) {
+    return population / worldPopulation * 100;
+}
+const populationPercentIreland1 = percentageOfWorld1(5);
+const populationPercentFinland1 = percentageOfWorld1(6);
+const populationPercentPortugal1 = percentageOfWorld1(12);
+const populationPercentIreland2 = percentageOfWorld2(5);
+const populationPercentFinland2 = percentageOfWorld2(6);
+const populationPercentPortugal2 = percentageOfWorld2(12)
+
+console.log(populationPercentFinland1, populationPercentFinland2);
+console.log(populationPercentIreland1, populationPercentIreland2);
+console.log(populationPercentPortugal1, populationPercentPortugal2);
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+/*
 // Strict mode
 let hasDriversLicence = false;
 const passTest = true;
@@ -31,3 +32,23 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+*/
+
+//  Function Declarations vs Expressions
+
+// Function Declaration (can be called before declaration)
+
+const age1 = calcAge1(1983);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;;
+}
+// Function Expression (can not be called before declaration)
+
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1983);
+
+console.log(age1, age2);
